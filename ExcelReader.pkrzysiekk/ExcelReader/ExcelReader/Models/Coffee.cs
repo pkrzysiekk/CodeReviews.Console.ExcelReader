cb695c3be8c311bfede8cd1dfa1a07723a0ab465
pkrzysiek <pkrzysiek24@gmail.com>
2025-07-03 19:16:14 +0200

    Add Id to the coffee model

--- ExcelReader.pkrzysiekk/ExcelReader/ExcelReader/Models/Coffee.cs
+++ ExcelReader.pkrzysiekk/ExcelReader/ExcelReader/Models/Coffee.cs
@@ -2,6 +2,7 @@ namespace ExcelReader.Models;
 
 public class Coffee
 {
+    public int Id { get; set; }
     public required string Name { get; set; }
     public required string Origin { get; set; }
     public required string RoastLevel { get; set; }
