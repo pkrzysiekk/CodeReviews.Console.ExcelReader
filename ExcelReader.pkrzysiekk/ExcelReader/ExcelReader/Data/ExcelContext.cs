d32cfeb74f528765a91a13027faa605f54acea5c
pkrzysiek <pkrzysiek24@gmail.com>
2025-07-03 13:56:27 +0200

    Add sqlLite configuration

--- ExcelReader.pkrzysiekk/ExcelReader/ExcelReader/Data/ExcelContext.cs
+++ ExcelReader.pkrzysiekk/ExcelReader/ExcelReader/Data/ExcelContext.cs
@@ -1,9 +1,14 @@
+using ExcelReader.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace ExcelReader.Data;
 
 public class ExcelContext : DbContext
 {
+    public DbSet<Coffee> Coffees { get; set; }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)=>
+        optionsBuilder.UseSqlite("Data Source=coffee.db");
     
     
 }
