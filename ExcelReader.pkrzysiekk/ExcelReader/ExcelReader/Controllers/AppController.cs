9308207543561993a888a25b221ef78fdb1df894
pkrzysiek <pkrzysiek24@gmail.com>
2025-07-03 19:38:51 +0200

    Update Controller

--- ExcelReader.pkrzysiekk/ExcelReader/ExcelReader/Controllers/AppController.cs
+++ ExcelReader.pkrzysiekk/ExcelReader/ExcelReader/Controllers/AppController.cs
@@ -1,6 +1,26 @@
+using EFCore.BulkExtensions;
+using ExcelReader.Data;
+using ExcelReader.Models;
+
 namespace ExcelReader.Controllers;
 
 public class AppController
 {
+    private ExcelContext _context;
+    private IExcelFileReader<Coffee> _fileReader; 
+    public AppController(ExcelContext context, IExcelFileReader<Coffee> fileReader)
+    {
+       _context = context; 
+       _fileReader = fileReader;
+    }
+
+    public void Run()
+    {
+        _context.Initialize();
+        var coffees = _fileReader.Read();
+        _context.BulkInsert(coffees);
+        _context.SaveChanges();
+
+    }
     
 }
